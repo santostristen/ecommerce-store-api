d67eea577e714f55080008b130d425454b519690
TavvyCat <tavish.cary@welch.edu>
2020-11-25 08:36:57 -0600

    Imports and new fle for product_routes

---
+++ app/routes/product_routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const customErrors = require('../../lib/custom_errors')
+
+const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+
+const removeBlanks = require('../../lib/remove_blank_fields')
+const requireToken = passport.authenticate('bearer', { session: false })
+
+const router = express.Router()
+
+router.get('/products', requireToken, (req, res, next) => {
+  
+})
